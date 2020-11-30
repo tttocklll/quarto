b0bc420e0517f9af15ca3cd98afebf5e7d11b924
OHMURA Tokiya <tocky.dison@gmail.com>
2020-11-30 15:26:14 +0900

    feat: detect clicked object

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int[,,] squares = new int [4, 10, 10];
+
+    // camera
+    private Camera camera_object;
+    private RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera_object = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            // Ray ray = camera_object.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+                int x = (Mathf.RoundToInt(hit.point.x) + 3) / 2;
+                int z = (Mathf.RoundToInt(hit.point.z) + 3) / 2;
+                Debug.Log(x);
+                Debug.Log(z);
+            }
+        }
+    }
+}
